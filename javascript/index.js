492d390fa53f6f63145662e894b608459d97aba3
vipul-027 <91328423+vipul-027@users.noreply.github.com>
2022-05-29 13:33:57 +0530

    https://github.com/vipul-027/learning-web-dev.io.git

---
+++ javascript/index.js
@@ -0,0 +1,29 @@
+//1ways to print in javascript
+// console.log("hello world-");
+//alert("me");
+// document.write("this is document write ")
+//2. javascript console API
+console.log("hello world", 4 + 6, "another log");
+console.warn("this is warning");
+console.error("this is an error");
+// 3. javascript variables
+// what are the variables ?- containers to store data 
+var number1=34; var number2= 56;
+console.log(number1+ number2);
+/*
+ multi line comment
+ */
+// 4. data types in javascript
+// numbers
+var num1=4556;
+var num2= 56.884;
+// string
+ var str1="this is a string";
+ var str2='this is also a string';
+ // objects
+  var marks= {
+      harrypotter:43,
+      sanjay:99,
+      vipul:99.999,
+  }
+  console.log(marks)
